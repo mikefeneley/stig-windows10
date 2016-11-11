c2f0a9f1597895505eccaed16163c58878980ac7
Michael <michael.j.feneley@gmail.com>
2016-11-11 00:21:54 -0500

    Comments on the winreg comaprator

--- src/winreg_comparator.py
+++ src/winreg_comparator.py
@@ -8,13 +8,20 @@ class WinRegComparator:
 	def reg_equals(self, computer_name=None, key=None, subkey=None, key_val=None, val=-1):
 		
 		"""
-		
-        :param hive: The default directory to search for the properties file
-        :type direc: string
-        :param key: The name of the default properties file
-        :type key: string
-        :returns: int -- 1 if the file is found, 0 otherwise
+		Check if a key with a specific value exists in the windows registry.
 
+
+        :param computer_name: The computer whose operating system we are checking.
+        :type computer_name: 
+        :param key: The predefined handle to connect to.
+        :type key: predefined key handle constant
+        :param subkey: Name of the sub key we want to open
+        :type subkey: String
+        :param key_val: Value in the subkey we want to query
+        :type key_val:	String
+        :param val: Value we are checking the registry for
+        :type val: int
+        :returns: int -- True if the registry value exists and equals val. False otherwise.
 		"""
 		try:
 			aReg  = ConnectRegistry(None, key)
